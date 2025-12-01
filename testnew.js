2b63374127cf29757da4223c4acccb9e5fca3293
Lovishka Soni <lovishka250@gmail.com>
2025-12-01 20:18:40 +0530

    updated the test file as per new features

--- testnew.js
+++ testnew.js
@@ -1,3 +1,35 @@
-if (a == b) {
-  console.log("debugging");
+var  x  = 5    
+var y= 10
+if  x == y {
+console.log("Equal")  
 }
+
+function hello () 
+{
+    console.log(  "Hello World"  )    
+}
+
+var obj = {
+name: "Lovishka"
+age: 19   
+country:"India"    
+}
+
+if x > 3{
+    console.log("x is big")    
+}
+
+if  y!=20{
+    console.log("not twenty" )  
+}
+
+function add(a,b){
+  return a  +  b  
+}
+
+console.log("Debug message");
+
+var unused   = 42   
+
+let message   =  "Hi"    
+
